58f830c567adfd267162f6d6f5550cfaa6626d73
Vicente Gutiérrez <gtz.rvera@gmail.com>
2021-06-09 15:05:18 -0500

    Add seeder to create the main user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \DB::table('users')->insert([
+            'name' => 'Vicente Gutiérrez',
+            'email' => 'thisisnotmyemail@email.com',
+            'password' => Hash::make('thisisnotmypassword')
+        ]);
+    }
+}
