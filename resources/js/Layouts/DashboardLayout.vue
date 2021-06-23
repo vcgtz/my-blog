97dc4be2f146db36adda69a3b986703faa6cf2ff
Vicente Gutiérrez <gtz.rvera@gmail.com>
2021-06-23 08:11:08 -0500

    Fix styles in the dashboard layout

--- resources/js/Layouts/DashboardLayout.vue
+++ resources/js/Layouts/DashboardLayout.vue
@@ -3,7 +3,7 @@
     <div class="min-h-screen h-full flex flex-no-wrap bg-gray-200">
       <dashboard-navigator></dashboard-navigator>
 
-      <div class="container mx-auto py-10 h-64 md:w-4/5 w-11/12 px-6">
+      <div class="h-auto container mx-auto py-10 md:w-4/5 w-11/12 px-6">
         <!-- Remove class [ border-dashed border-2 border-gray-300 ] to remove dotted border -->
         <div class="w-full h-full border-gray-300">
           <slot></slot>
